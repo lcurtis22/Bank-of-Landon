52fd7fd8a6ad4a2ba6e8cfdfeb5208f57677c3bf
lcurtis541 <70545873+lcurtis541@users.noreply.github.com>
2021-09-15 19:16:46 -0600

    Testing HTML Body changing
    Trying to figure out how to change the body of the html page with each different click.

--- js/button.js
+++ js/button.js
@@ -3,3 +3,8 @@ function buttonChange(button) {
 
   document.getElementById("buttonText").innerHTML  = "<vfont>Selected button:" + button + "</vfont>";
 }
+
+function buttonChangePage(){
+	document.body.innerHTML += "<center><h2><vfont>Login:</vfront></h2></center>";
+	document.body.innerHTML += "<center><h3><vfont>Password:</vfront></h2></center>";
+}
